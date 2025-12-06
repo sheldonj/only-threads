4d100397266e86bbcdbb85118c3a84a51de7ae44
James Sheldon <sheldonj@gmail.com>
2025-12-06 10:46:17 -0700

    Update dependencies in package.json and pnpm-lock.yaml, including @hookform/resolvers, @radix-ui components, react-hook-form, and others. Refactor UI components to improve code consistency and readability, including Accordion, AlertDialog, Avatar, and more.

--- components/ui/password-input.tsx
+++ components/ui/password-input.tsx
@@ -4,10 +4,10 @@ import { EyeIcon, EyeOffIcon } from "lucide-react";
 import * as React from "react";
 
 import { Button } from "@/components/ui/button";
-import { Input, type InputProps } from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 
-const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
+const PasswordInput = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 	({ className, ...props }, ref) => {
 		const [showPassword, setShowPassword] = React.useState(false);
 		const disabled =
